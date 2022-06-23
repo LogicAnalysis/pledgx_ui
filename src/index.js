119d13fab4f09c9b5b1dd1932085db961f9894d9
Ahmad Atiya <ahmad.m.atiya@gmail.com>
2022-06-23 10:46:26 -0400

    updated to RN18 create root standard

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 //import * as serviceWorker from './serviceWorker';
 //import reportWebVitals from './reportWebVitals';
 
 import './i18n';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
 	<React.StrictMode>
 		<Suspense fallback={(<div>Loading</div>)}>
 			<App useSuspense={true} />
 		</Suspense>
-	</React.StrictMode>,
-	document.getElementById('root')
+	</React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
