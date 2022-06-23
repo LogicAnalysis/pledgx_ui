b3755cd9b3a4cecfb9a1274b55b4c3c7c9933464
Ahmad Atiya <ahmad.m.atiya@gmail.com>
2022-06-23 12:36:44 -0400

    added navigation bar

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect, Fragment } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import Alerts from '../components/alerts/Alerts';
-
-import Main from '../components/pages/Main';
-
 import authContext from '../context/auth/authContext';
 import prefContext from '../context/pref/prefContext';
+
+import classes from './style/MainContainer.module.css';
+
+import Alerts from '../components/alerts/Alerts';
+import Main from '../components/pages/Main';
+import Navigation from '../components/navigation/Navigation'
 
 function MainContainer() {
 	const AuthContext = useContext(authContext);
@@ -23,9 +25,12 @@ function MainContainer() {
 	return (
 		<Fragment>
 			<Alerts />
+			<div className={ classes.navigation }>
+				<Navigation />
+			</div>
 			<Routes>
-				<Route exact path="/" render={() => <Navigate to="/main" />} />
-				<Route exact path='/main' component={ Main } />
+				<Route exact path='/main' element={ <Main /> } />
+				<Route exact path="*" element={<Navigate to="/main" />} />
 			</Routes>
 		</Fragment>
 	);
