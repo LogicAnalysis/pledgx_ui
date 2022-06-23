ad351f96a954e1ebfbded58a3b2e8215caa88a04
Ahmad Atiya <ahmad.m.atiya@gmail.com>
2022-06-23 10:35:38 -0400

    fixed incorrect color names

--- src/theme/Colors.js
+++ src/theme/Colors.js
@@ -1,9 +1,9 @@
 // Primary Colors
 export const DARK_COLOR_DARK = "#222";
-export const LIGHT_COLOR_LIGHT_2 = "#A2A2A2";
+export const LIGHT_COLOR_LIGHT_1 = "#F8F8F8";
 
-export const MAIN_BACKGROUND_COLOR_LIGHT = '#FFFFFF';
-export const MAIN_BACKGROUND_COLOR_DARK = '#000000';
+export const MAIN_TEXT_COLOR_LIGHT = '#707070';
+export const MAIN_TEXT_COLOR_DARK = '#999999';
 
 // Additional Colors
 
